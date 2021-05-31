ee3075d69b58b2b7c5ddc0283fb10e2126d7c2f6
pyt111 <584702675@qq.com>
2021-05-31 14:26:01 +0800

    list

--- pages/item/item.vue
+++ pages/item/item.vue
@@ -2,7 +2,7 @@
 	<view>
 		<view class="uni-header">
 			<view class="uni-group hide-on-phone">
-				<view class="uni-title">任务列表</view>
+				<view class="uni-title">项目列表</view>
 				<view class="uni-sub-title"></view>
 			</view>
 			<view class="uni-group">
@@ -12,11 +12,11 @@
 		</view>
 		<view class="uni-container">
 			<uni-list>
-			    <uni-list-item title="柳州科技馆柳州科技馆柳州科技馆" link to="/pages/item/detail" rightText="2021年4月22日"></uni-list-item>
-			    <uni-list-item title="遂宁展馆遂宁展馆遂宁展馆遂宁展馆" link to="/pages/item/detail" rightText="2021年4月20日"></uni-list-item>
-			    <uni-list-item title="咸阳市科技馆咸阳市科技馆咸阳市科技馆" link to="/pages/item/detail" rightText="2021年4月19日"></uni-list-item>
-			    <uni-list-item title="宝鸡市科技馆宝鸡市科技馆宝鸡市科技馆" link to="/pages/item/detail" rightText="2021年4月18日"></uni-list-item>
-			    <uni-list-item title="四川省科技馆四川省科技馆四川省科技馆四川省科技馆" link to="/pages/item/detail" rightText="2021年4月17日"></uni-list-item>
+			    <uni-list-item title="柳州科技馆柳州科技馆柳州科技馆" link to="/pages/item/list" rightText="2021年4月22日"></uni-list-item>
+			    <uni-list-item title="遂宁展馆遂宁展馆遂宁展馆遂宁展馆" link to="/pages/item/list" rightText="2021年4月20日"></uni-list-item>
+			    <uni-list-item title="咸阳市科技馆咸阳市科技馆咸阳市科技馆" link to="/pages/item/list" rightText="2021年4月19日"></uni-list-item>
+			    <uni-list-item title="宝鸡市科技馆宝鸡市科技馆宝鸡市科技馆" link to="/pages/item/list" rightText="2021年4月18日"></uni-list-item>
+			    <uni-list-item title="四川省科技馆四川省科技馆四川省科技馆四川省科技馆" link to="/pages/item/list" rightText="2021年4月17日"></uni-list-item>
 			</uni-list>
 		</view>
 	</view>
